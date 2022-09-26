ebf8887a9a7494b29523518c1db5550c040bfd6e
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-09-26 20:06:56 +0530

    Add background img for empty todo list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './styles/main.css';
 import './styles/nav.css';
+import sadBluey from './assets/sad-bluey.png';
+
+const setEmptyTodoBackground = () => {
+  const main = document.querySelector('main');
+  const emptyTodoBgImg = document.createElement('img');
+  emptyTodoBgImg.classList.add('bg-img');
+  emptyTodoBgImg.src = sadBluey;
+  const emptyTodoText = document.createElement('p');
+  emptyTodoText.innerText = 'Nothing yet...';
+  main.innerText = '';
+  main.appendChild(emptyTodoText);
+  main.appendChild(emptyTodoBgImg);
+};
+
+setEmptyTodoBackground();
