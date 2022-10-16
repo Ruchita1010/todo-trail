9da9efa7138bd1284361f49daa603de6a197b51b
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-10-16 14:43:37 +0530

    Get user input and display it in the DOM

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -8,15 +8,75 @@ const toggleDisplay = (elemClass, elem) => {
   wrapper.classList.toggle(`show-${elem}`);
 };
 
-function closeModal(e) {
+const closeModal = (e) => {
   const clickedBtn = e.target.classList[2];
-  if (clickedBtn === 'project-close-btn') {
+  if (clickedBtn === 'project-modal-btn') {
     toggleDisplay('project-modal', 'modal');
   } else {
     toggleDisplay('todo-modal', 'modal');
   }
   toggleDisplay('modal-wrapper', 'modal');
-}
+};
+
+const updateMain = (elem) => {
+  const main = document.querySelector('main');
+  main.appendChild(elem);
+};
+
+const getUserInputs = (parentModalElem) => {
+  const userInputDivs = getSimilarClassElements(
+    `.${parentModalElem} .user-input`
+  );
+  return [...userInputDivs].map((userInput) => userInput.children[1].value);
+};
+
+const createTodo = () => {
+  const [title, description, date, project, priority] =
+    getUserInputs('todo-modal');
+  const newTodo = document.createElement('div');
+  newTodo.classList.add('todo');
+  newTodo.innerHTML = ` <p>Title ${title}</p>
+        <p>Project ${project}</p>
+        <p>Due Date ${date}</p>
+        <p>Priority ${priority}</p>
+        <p>Desc ${description}</p>`;
+  return newTodo;
+};
+
+const addTodo = () => {
+  const newTodo = createTodo();
+  updateMain(newTodo);
+};
+
+const createProject = (title) => {
+  const newProject = document.createElement('div');
+  newProject.classList.add('project');
+  newProject.innerHTML = `<p>Title ${title}</p>`;
+  return newProject;
+};
+
+const addProject = () => {
+  const [title] = getUserInputs('project-modal');
+  const newProject = createProject(title);
+  updateMain(newProject);
+};
+
+const addUserInputData = (e) => {
+  const clickedBtn = e.target.classList[2];
+  if (clickedBtn === 'project-modal-btn') {
+    addProject();
+  } else {
+    addTodo();
+  }
+  closeModal(e);
+};
+
+const listenForDoneBtns = () => {
+  const doneBtns = getSimilarClassElements('done-btn');
+  doneBtns.forEach((doneBtn) => {
+    doneBtn.addEventListener('click', addUserInputData);
+  });
+};
 
 const listenForCloseBtnClick = () => {
   const closeBtns = getSimilarClassElements('close-btn');
@@ -36,6 +96,7 @@ const showCreateOption = (e) => {
   } else {
     toggleDisplay('project-modal', 'modal');
   }
+  listenForDoneBtns();
   listenForCloseBtnClick();
 };
 
